67b052d2cd48049e996a13570568e9be59e1b8af
LucasSBarros <101149312+LucasSBarros@users.noreply.github.com>
2026-02-11 02:12:08 -0300

    (Sprint 0) #Commit inicial

---
+++ src/test/java/com/calorietracker/Calorie_Tracker/CalorieTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calorietracker.Calorie_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
